4cb994be8e395a0d3d6df85ef74097377555a8a6
Evander Kristalino <christrevand@gmail.com>
2021-01-22 14:00:41 +0800

    added build folder

--- src/config/urlAPI.jsx
+++ src/config/urlAPI.jsx
@@ -1,1 +1,2 @@
-export const urlAPI = 'https://evander-portfolio-api.herokuapp.com'
+// export const urlAPI = 'https://evander-portfolio-api.herokuapp.com'
+export const urlAPI = 'http://192.168.1.10:2000'
