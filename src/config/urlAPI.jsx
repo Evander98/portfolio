c375fee3c0caa6a23bbf2de9db3e05ea4e8e2b09
Evander Kristalino <christrevand@gmail.com>
2021-01-22 12:19:18 +0800

    title changed

--- src/config/urlAPI.jsx
+++ src/config/urlAPI.jsx
@@ -1,2 +1,1 @@
-// export const urlAPI = 'https://evander-portfolio-api.herokuapp.com'
-export const urlAPI = 'http://192.168.1.10:2000'
+export const urlAPI = 'https://evander-portfolio-api.herokuapp.com'
